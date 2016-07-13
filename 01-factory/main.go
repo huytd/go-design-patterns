66a9cef942e8457ebebaf0d7da34aeaeda934e37
Dung Quang <nxqd.inbox+rhacker@gmail.com>
2016-07-13 09:04:21 +0700

    Update main.go

--- 01-factory/main.go
+++ 01-factory/main.go
@@ -4,55 +4,64 @@ import "fmt"
 
 // Shape interface
 type Shape interface {
-  Draw()
+	Draw()
 }
 
 // Rectangle class implementing Shape interface
-type Rectangle struct { }
+type Rectangle struct{}
 
 func (r *Rectangle) Draw() {
-  fmt.Print("A rect is drawing\n")
+	fmt.Print("A rect is drawing\n")
 }
 
 // Circle class implementing Shape interface
-type Circle struct { }
+type Circle struct{}
 
 func (c *Circle) Draw() {
-  fmt.Print("A circle is drawing\n")
+	fmt.Print("A circle is drawing\n")
 }
 
 // Square class implementing Shape interface
-type Square struct { }
+type Square struct{}
 
 func (s *Square) Draw() {
-  fmt.Print("A square is drawing\n")
+	fmt.Print("A square is drawing\n")
 }
 
 // ShapeFactory is a factory to generate class based on given name
-type ShapeFactory struct { }
+type ShapeFactory struct{}
 
-func (sf *ShapeFactory) GetShape(shapeType string) Shape {
-  if shapeType == "CIRCLE" {
-    return &Circle{}
-  } else if shapeType == "RECTANGLE" {
-    return &Rectangle{}
-  } else if shapeType == "SQUARE" {
-    return &Square{}
-  } else {
-    return nil
-  }
+// ShapeType Nay thi comment
+type ShapeType int
+
+const (
+	CIRCLE ShapeType = iota
+	RECTANGLE
+	SQUARE
+)
+
+func (sf *ShapeFactory) GetShape(shapeType ShapeType) Shape {
+	if shapeType == CIRCLE {
+		return &Circle{}
+	} else if shapeType == RECTANGLE {
+		return &Rectangle{}
+	} else if shapeType == SQUARE {
+		return &Square{}
+	} else {
+		return nil
+	}
 }
 
 // Demo program
 func main() {
-  factory := ShapeFactory{}
+	factory := ShapeFactory{}
 
-  circle := factory.GetShape("CIRCLE")
-  circle.Draw()
+	circle := factory.GetShape(CIRCLE)
+	circle.Draw()
 
-  rectangle := factory.GetShape("RECTANGLE")
-  rectangle.Draw()
+	rectangle := factory.GetShape(RECTANGLE)
+	rectangle.Draw()
 
-  square := factory.GetShape("SQUARE")
-  square.Draw()
+	square := factory.GetShape(SQUARE)
+	square.Draw()
 }
